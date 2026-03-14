f6df48afb57671c60d7fa5e1f8dca7e92d68abdb
Joshua Argyle <joshbudd33@gmail.com>
2026-03-13 18:42:49 -0600

    Added functionality to vault page so it will place favorite and missed questions there from local storage.

---
+++ vault.js
@@ -0,0 +1,27 @@
+
+
+function displayFavoritesVault() {
+  const list = JSON.parse(localStorage.getItem("favorites")) || [];
+  const vault = document.querySelector(".favoritesVault");
+
+  if (list.length === 0) {
+    vault.innerHTML = `<p>No favorite questions yet...</p>`;
+  } else {
+    vault.innerHTML = list.map(q => `<p class="favoriteQuestion">${q.question} - <span class="correct">${q.correct_answer}</span</p>`).join("");
+  }
+}
+
+
+function displayMissedVault() {
+  const missed = JSON.parse(localStorage.getItem("missed")) || [];
+  const vault = document.querySelector(".missedVault");
+
+  if (missed.length === 0) {
+    vault.innerHTML = `<p>No missed questions yet...</p>`;
+  } else {
+    vault.innerHTML = missed.map(q => `<p class="missed">${q.question} - <span class="missedAnswer">${q.correct_answer}</span</p>`).join("");
+  }
+}
+
+displayFavoritesVault();
+displayMissedVault();
