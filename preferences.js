6a36b12a8bcf35c763401b9a600ff359177ee7ee
Joshua Argyle <joshbudd33@gmail.com>
2026-04-01 13:52:29 -0600

    Add functionality to preferences so that it filters the categories based on the users pick. As of right now it only picks a category for the whole game at random, not different of those selected categories for each question.

--- preferences.js
+++ preferences.js
@@ -1,9 +1,87 @@
 const pref = document.querySelector('.set-pref');
 const button = document.querySelector('.pref-button');
 const arrow = document.querySelector('.arrow');
+const form = document.querySelector('.form');
+const categoryContainer = document.querySelector('#categories-toggled');
+const categoryLookup = {
+    "9": "General Knowledge",
+    "10": "Books",
+    "11": "Film",
+    "12": "Music",
+    "13": "Musicals & Theatre",
+    "14": "Television",
+    "15": "Video Games",
+    "16": "Board Games",
+    "17": "Science & Nature",
+    "18": "Computers",
+    "19": "Mathematics",
+    "20": "Mythology",
+    "21": "Sports",
+    "22": "Geography",
+    "23": "History",
+    "24": "Politics",
+    "25": "Art",
+    "26": "Celebrities",
+    "27": "Animals",
+    "28": "Vehicles",
+    "29": "Comics",
+    "30": "Gadgets",
+    "31": "Japanese Anime & Manga",
+    "32": "Cartoon & Animations"
+};
+
 
 button.addEventListener('click', () => {
   pref.classList.toggle("hidden");
   arrow.classList.toggle('arrow-is-rotated');
 });
 
+
+form.addEventListener('submit', function(event) {
+  localStorage.removeItem('userData');
+  event.preventDefault()
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData.entries());
+
+  data.category = formData.getAll('category');
+
+  localStorage.setItem('userData', JSON.stringify(data));
+
+  updateUI();
+})
+
+function updateUI() {
+
+  categoryContainer.innerHTML = '';
+  
+  const savedData = localStorage.getItem('userData');
+
+  if (savedData) {
+    
+    const userPrefs = JSON.parse(savedData);
+
+    
+    const selectedCategories = userPrefs.category; 
+    selectedCategories.forEach(cat => {
+  
+      const li = document.createElement('li');
+      li.classList.add('category-item');
+
+      const categoryName = categoryLookup[cat]
+    
+      li.textContent = categoryName;
+    
+      categoryContainer.appendChild(li);
+    });
+
+  }
+  else {
+    const li = document.createElement('li');
+      li.classList.add('category-item');
+    
+      li.textContent = "Everything is currently toggled on.";
+    
+      categoryContainer.appendChild(li);
+  }
+};
+updateUI();
